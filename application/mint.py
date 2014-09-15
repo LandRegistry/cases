6fc035bea999e9bc5ab7611b3a400e999e426707
Juan Matthys Uys <opyate@gmail.com>
2014-09-15 12:23:51 +0100

    post to mint

--- application/mint.py
+++ application/mint.py
@@ -20,12 +20,12 @@ class Mint(object):
         else:
             self.auth = None
 
-def post_to_mint(self, url, data, title_number):
-    title_url = '%stitles/%s' % (url, title_number)
-    logger.info("Sending data %s to the mint at %s" % (data, title_url))
-    # try:
-    #     response = requests.post(title_url, data=json.dumps(data), headers=headers)
-    #     return response
-    # except requests.exceptions.RequestException as e:
-    #     logger.error("Could not create title number %s: Error %s" % (title_url, e))
-    #     raise RuntimeError
+    def post(self, url, data, title_number):
+        title_url = '%stitles/%s' % (url, title_number)
+        logger.info("Sending data %s to the mint at %s" % (data, title_url))
+        try:
+            response = requests.post(title_url, data=json.dumps(data), headers=headers)
+            return response
+        except requests.exceptions.RequestException as e:
+            logger.error("Could not create title number %s: Error %s" % (title_url, e))
+            raise RuntimeError
--- environment.sh
+++ environment.sh
@@ -1,4 +1,4 @@
 export SETTINGS='config.DevelopmentConfig'
 export DATABASE_URL='postgresql://localhost/cases'
 export DECISION_URL='http://decision.landregistry.local'
-export MINT_URL='http://mint.landregistry.local'
+export MINT_URL='http://mint.landregistry.local/'
